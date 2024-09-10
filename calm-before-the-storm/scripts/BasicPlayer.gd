f882a7dd1a1e1c9e02a58428f97aeff2a40de6ed
WK-Kz <molaskendall@gmail.com>
2024-09-09 18:05:55 -0700

    - Tile sheet tested. - Player movement has been implemented alongside a test scene

--- calm-before-the-storm/scripts/BasicPlayer.gd
+++ calm-before-the-storm/scripts/BasicPlayer.gd
@@ -6,8 +6,6 @@ var stamina : int = 100
 @export var ACCELERATION = 1000
 @export var FRICTION = 5
 
-# delta is not being used in calculation for velocity, we want the player to be moving at a constant speed all the time
-# in addition, delta is automatically used within the move and slide code
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
