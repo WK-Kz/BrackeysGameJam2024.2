87ea15f609672cc69098e8dec485db021d7690d5
WK-Kz <molaskendall@gmail.com>
2024-09-11 03:06:39 -0700

    This commit includes modification of the DialogueManager along with a test scene with the existing NPC
    The modification of the DialogueManager was required so that we can get
    the "interact" key to work properly with dialogue.
    
    In addition, hooked up the DialogueManager's on_dialogue_end_signal to
    prevent the character from moving when choosing responses from the
    dialogue.
    
    Added a new test scene with the NPC existing and working with the
    dialogue responses.

--- calm-before-the-storm/scripts/Player/BasicPlayer.gd
+++ calm-before-the-storm/scripts/Player/BasicPlayer.gd
@@ -2,26 +2,20 @@ extends CharacterBody2D
 
 var stamina : int = 100
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var interaction_area: Area2D = $PlayerDirection/InteractionArea
 
 @export var SPEED = 400
 @export var ACCELERATION = 1000
 @export var FRICTION = 5
 
-enum AVAILABLE_ACTIONS
-{
-	WALK,
-	INTERACT
-}
-
-enum INTERACT_OPTIONS
-{
-	TALK,
-	MOVE_BOX,
-	PRESS_BUTTON,
-	SWITCH_LEVER
-}
+var can_player_move : bool = true
 
 func _physics_process(delta: float) -> void:
+	if !can_player_move:
+		return
+	else:
+		DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
 	if direction:
@@ -34,3 +28,15 @@ func _physics_process(delta: float) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta_: float) -> void:
 	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		var interactions = interaction_area.get_overlapping_areas()
+		if interactions.size() > 0:
+			var resulting_action = interactions[0].interact()
+			if (resulting_action == PlayerBehavior.INTERACTIONS.TALK):
+				can_player_move = false
+				
+func _on_dialogue_ended(_resource: DialogueResource):
+	can_player_move = true
+	
